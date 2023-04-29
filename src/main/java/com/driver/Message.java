6edaac1ce3d8a75be59a5464e952e4c743decf6b
Shijin-Varghese <108483342+Shijin-Varghese@users.noreply.github.com>
2023-04-29 13:26:46 +0530

    hell

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -8,10 +8,4 @@ public class Message {
     private int id;
     private String content;
     private Date timestamp;
-
-    public Message(int id, String content, Date timestamp) {
-        this.id = id;
-        this.content = content;
-        this.timestamp = timestamp;
-    }
 }
